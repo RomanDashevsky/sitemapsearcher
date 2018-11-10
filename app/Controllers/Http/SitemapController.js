669350305454a65c1ed27eced536cc06a29ef922
Roman Dashevsky <rdashevsky@lineate.com>
2018-11-11 01:04:35 +0600

    get array of url from sitemap.xml

--- app/Controllers/Http/SitemapController.js
+++ app/Controllers/Http/SitemapController.js
@@ -1,14 +1,32 @@
 'use strict'
 
+const Drive = use('Drive')
+var parseString = require('xml2js').parseString
+
+const getUrlFromSiteMap = async () => {
+  console.log(__dirname)
+  const siteMapXML = await Drive.get(__dirname + '/../../../sitemap.xml')
+
+  // cover xml2js by promise, make it async/await like
+  const siteMapJSON = await new Promise((resolve, reject) => parseString(siteMapXML, function (err, result) {
+    if (err) reject(err)
+    else resolve(result)
+  }))
+
+  const urlArray = siteMapJSON.urlset.url.map(function callback(currentValue, index, array) {
+    return currentValue.loc[0]
+  })
+
+  return urlArray
+}
+
 class SitemapController {
 
-  index({ request, response }) {
+  async index({ request, response }) {
 
+    const urlArray = await getUrlFromSiteMap()
 
-
-    response.json({
-      massage: 'searching...'
-    });
+    response.json(urlArray)
 
   }
 
