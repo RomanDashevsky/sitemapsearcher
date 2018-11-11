f066aa43e1c98fcbb86c093f1b046e7ed6a7bbbd
Roman Dashevsky <rdashevsky@lineate.com>
2018-11-11 17:35:27 +0600

    add search logic

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ This is tool for finding html elements at URLs from sitemap.xml
 ## Setup
 
 npm i
+
+## RUN
+
+adonis serve --dev
--- app/Controllers/Http/SitemapController.js
+++ app/Controllers/Http/SitemapController.js
@@ -1,10 +1,16 @@
 'use strict'
 
 const Drive = use('Drive')
-var parseString = require('xml2js').parseString
+const parseString = require('xml2js').parseString
+const { Builder, By, until } = require('selenium-webdriver');
+const WebDriver = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const path = require('chromedriver').path;
+const service = new chrome.ServiceBuilder(path).build();
+chrome.setDefaultService(service);
 
 const getUrlFromSiteMap = async () => {
-  console.log(__dirname)
+
   const siteMapXML = await Drive.get(__dirname + '/../../../sitemap.xml')
 
   // cover xml2js by promise, make it async/await like
@@ -18,16 +24,50 @@ const getUrlFromSiteMap = async () => {
   })
 
   return urlArray
+
+}
+
+const getPagesWithSearchingContent = async (urlArray, searchWord) => {
+
+  let driver = await new WebDriver.Builder()
+    .withCapabilities(WebDriver.Capabilities.chrome())
+    .build();
+  const pages = [];
+
+  try {
+    for (const url of urlArray) {
+      await driver.get(url)
+      // await driver.wait(until.titleIs('mock title, just wait'), 2000)
+      const element = await driver.findElement(By.css('html'))
+      const html = await element.getAttribute('innerHTML')
+
+      if (html.indexOf(searchWord) >= 0) {
+        console.log(url);
+        pages.push(url)
+      }
+    }
+  } catch (e) {
+    throw e
+  } finally {
+    driver.quit()
+  }
+
+  return pages
+
+}
+
+const runSearch = async (searchWord) => {
+  const urlArray = await getUrlFromSiteMap()
+  const result = await getPagesWithSearchingContent(urlArray, searchWord)
+  await Drive.put('result.json', {'searchWord': searchWord, 'result': result})
 }
 
 class SitemapController {
 
   async index({ request, response }) {
-
-    const urlArray = await getUrlFromSiteMap()
-
-    response.json(urlArray)
-
+    const searchWord = request._qs.word
+    runSearch(searchWord)
+    response.json({'message': `Start searching '${searchWord}'`})
   }
 
 }
