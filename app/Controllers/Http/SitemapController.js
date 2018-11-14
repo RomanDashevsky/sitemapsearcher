d0fe261888896a3e24ebc0d49633b043474bae30
Roman Dashevsky <rdashevsky@lineate.com>
2018-11-14 18:16:57 +0600

    add multi thread logic

--- app/Controllers/Http/SitemapController.js
+++ app/Controllers/Http/SitemapController.js
@@ -1,13 +1,15 @@
 'use strict'
 
 const Drive = use('Drive')
+const Env = use('Env')
+const countOfProcesses = Env.get('COUNT_OF_PROCESSES')
 const parseString = require('xml2js').parseString
-const { Builder, By, until } = require('selenium-webdriver');
-const WebDriver = require('selenium-webdriver');
-const chrome = require('selenium-webdriver/chrome');
-const path = require('chromedriver').path;
-const service = new chrome.ServiceBuilder(path).build();
-chrome.setDefaultService(service);
+const { Builder, By, until } = require('selenium-webdriver')
+const WebDriver = require('selenium-webdriver')
+const chrome = require('selenium-webdriver/chrome')
+const path = require('chromedriver').path
+const service = new chrome.ServiceBuilder(path).build()
+chrome.setDefaultService(service)
 
 const getUrlFromSiteMap = async () => {
 
@@ -27,29 +29,56 @@ const getUrlFromSiteMap = async () => {
 
 }
 
+const searchWordOnPage = async (url, searchWord, driver, pages) => {
+
+  await driver.get(url)
+  const element = await driver.findElement(By.css('html'))
+  const html = await element.getAttribute('innerHTML')
+
+  if (html.indexOf(searchWord) >= 0) {
+    console.log(url);
+    pages.push(url)
+  }
+
+}
+
 const getPagesWithSearchingContent = async (urlArray, searchWord) => {
 
-  let driver = await new WebDriver.Builder()
-    .withCapabilities(WebDriver.Capabilities.chrome())
-    .build();
-  const pages = [];
+  const driverPool = [];
+
+  for (let i = 0; i < countOfProcesses; i++) {
+    const driver = await new WebDriver.Builder()
+      .withCapabilities(WebDriver.Capabilities.chrome())
+      .build()
+    driverPool.push(driver)
+  }
+
+  const pages = []
 
   try {
-    for (const url of urlArray) {
-      await driver.get(url)
-      // await driver.wait(until.titleIs('mock title, just wait'), 2000)
-      const element = await driver.findElement(By.css('html'))
-      const html = await element.getAttribute('innerHTML')
+    const countOfUrls = urlArray.length
+    let currentSearchingPromises = [];
 
-      if (html.indexOf(searchWord) >= 0) {
-        console.log(url);
-        pages.push(url)
+    currentSearchingPromises.push(searchWordOnPage(urlArray[0], searchWord, driverPool[0], pages))
+
+    for (let i = 1; i < countOfUrls; i++) {
+
+      currentSearchingPromises.push(searchWordOnPage(urlArray[i], searchWord, driverPool[i % countOfProcesses], pages))
+      if (i % countOfProcesses === 0) {
+        await Promise.all(currentSearchingPromises)
+        currentSearchingPromises = []
       }
+
     }
+
+    await Promise.all(currentSearchingPromises)
+
   } catch (e) {
     throw e
   } finally {
-    driver.quit()
+    for (const driver of driverPool) {
+      driver.quit()
+    }
   }
 
   return pages
