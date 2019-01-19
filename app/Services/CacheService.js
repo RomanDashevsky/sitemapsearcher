9e855635a07bd6feafcd3f0cb977c6f51432f5dc
Roman Dashevsky <rdashevsky@lineate.com>
2019-01-19 23:01:40 +0600

    fix redis store logic

--- app/Services/CacheService.js
+++ app/Services/CacheService.js
@@ -5,13 +5,12 @@ const Redis = use('Redis')
 class CacheService {
 
   static async set(key, value) {
-    await Redis.set(key, value)
+    await Redis.set(key, JSON.stringify(value))
   }
 
   static async get(key) {
-    return await Redis.get(key)
+    return JSON.parse(await Redis.get(key))
   }
-
 }
 
 module.exports = CacheService
